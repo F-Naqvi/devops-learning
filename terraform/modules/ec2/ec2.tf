e4ae52ab3be9c6317230ed6685f30d588c269cdc
F-Naqvi <fnaqvi8@icloud.com>
2026-09-02 14:32:33 +0100

    edit tf files

--- terraform/modules/ec2/ec2.tf
+++ terraform/modules/ec2/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "this" {
-  ami                     = "ami-0224ce6f9504665ee"
+  ami                     = local.instance_ami
   instance_type           = var.instance_type
 }
 
 resource "aws_instance" "ec2-import-test" {
-  ami                     = "ami-0224ce6f9504665ee"
+  ami                     = local.instance_ami
   instance_type           = var.instance_type
   tags = {
     Name = "terraform import"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,11 @@ terraform {
             version = "6.57.1"
         }
     }
+    #backend "s3" {
+      #bucket = "value"
+      #key = "value"
+      #region = "value"
+    #}
 }
 
 provider "aws" {
