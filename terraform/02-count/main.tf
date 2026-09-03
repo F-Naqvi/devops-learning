51f841ae39f4dd15326dcbe88922813813020f90
F-Naqvi <fnaqvi8@icloud.com>
2026-09-03 17:56:14 +0100

    02-count example

---
+++ terraform/02-count/main.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "6.57.1"
+        }
+    }
+}
+
+provider "aws" {
+    # Configuration options
+    access_key = "test"
+    secret_key = "test"
+    region = "eu-west-2"
+    skip_credentials_validation = "true"
+    skip_requesting_account_id = "true"
+
+    endpoints {
+      ec2 = "http://localhost:4566"
+    }
+}
+
+####### Count ######
+
+resource "aws_instance" "example" {
+  count = 2
+  ami                     = "ami_test"
+  instance_type           = "t2.micro"
+}
